6052c3fccb0eb82707536594064c3e0471e99c13
George-mFF4G <gm1362@fayoum.edu.eg>
2024-03-14 04:52:52 +0200

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import re
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 
-def scrape_stock_mentions(twitter_accounts, stock_symbol, interval_minutes):
+def scrape_stock_mentions(twitter_accounts, interval_minutes):
     try:
-        PATH = "./chromedriver.exe"  
+        PATH = "./chromedriver.exe"
         URL = "https://twitter.com/"
 
         chrome_options = webdriver.ChromeOptions()
@@ -20,27 +20,33 @@ def scrape_stock_mentions(twitter_accounts, stock_symbol, interval_minutes):
         driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.get("https://twitter.com")
 
+        total_mentions = {}
+
         while True:
-            total_mentions = 0
             for account in twitter_accounts:
                 try:
                     finalURL = f"{URL}{account}"
                     driver.get(finalURL)
-                    time.sleep(5)  
+                    time.sleep(5)
                     page_cards = driver.find_elements(By.XPATH, '//article[@data-testid="tweet"]')
                     for card in page_cards:
                         try:
                             tweet_text_element = card.find_element(By.XPATH, './/div[@dir="auto"]/span')
                             tweet_text = tweet_text_element.text
-                            if stock_symbol in tweet_text:
-                                total_mentions += 1
-                        except NoSuchElementException as e:
-                            print("Error parsing tweet text:", e)
+                            symbols = re.findall(r'\$(\w+)', tweet_text)
+                            for symbol in symbols:
+                                if symbol not in total_mentions:
+                                    total_mentions[symbol] = 1
+                                else:
+                                    total_mentions[symbol] += 1
+                        except NoSuchElementException:
+                            pass
 
                 except Exception as e:
                     print("Error scraping account:", e)
 
-            print(f"{stock_symbol} was mentioned {total_mentions} times in the last {interval_minutes} minutes.")
+            for symbol, count in total_mentions.items():
+                print(f"${symbol} was mentioned {count} times in the last {interval_minutes} minutes.")
             time.sleep(interval_minutes * 60)
 
     except Exception as e:
@@ -52,10 +58,9 @@ def scrape_stock_mentions(twitter_accounts, stock_symbol, interval_minutes):
 def main():
     twitter_accounts = ["Mr_Derivatives", "warrior_0719", "ChartingProdigy", "allstarcharts", "yuriymatso",
                         "TriggerTrades", "AdamMancini4 ", "CordovaTrades", "Barchart",
-                        "RoyLMattox"]  
-    stock_symbol = "$TSLA" 
-    interval_minutes = 15  
-    scrape_stock_mentions(twitter_accounts, stock_symbol, interval_minutes)
+                        "RoyLMattox"]
+    interval_minutes = 15
+    scrape_stock_mentions(twitter_accounts, interval_minutes)
 
 
 if __name__ == "__main__":
